abddf98b07ab56e4ceb664bf89cfcca2eec6e7a0
Jean-Claude Wippler <jc@wippler.nl>
2012-10-21 15:25:12 +0200

    fix value decompress bug

--- examples/homeGraph/homeGraph.ino
+++ examples/homeGraph/homeGraph.ino
@@ -1,4 +1,6 @@
-// Display latest home power consumption info.
+/// @dir homeGraph
+/// Display latest home power consumption info.
+/// @see http://jeelabs.org/2012/10/25/who-needs-a-server/
 // 2012-10-21 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
 
 #include <GLCD_ST7565.h>
@@ -21,25 +23,25 @@ GLCD_ST7565 glcd;
 
 // show a single power value on the graphics display
 static void showPower (word value, byte ypos) {
-  char buf[5];
-  if (value == 0 || value >= 65000)
-    strcpy(buf, "   -");
+  char buf[6];
+  if (value <= 100 || value >= 65000)
+    strcpy(buf, "    -");
   else {
     long ms = value;
     if (value > 60000)
       ms = 1000L * (value - 60000);
-    sprintf(buf, "%4lu", 1800000L / (long) value);
+    sprintf(buf, "%5lu", 1800000L / ms);
   }
-  glcd.drawString(90,  ypos, buf);
+  glcd.drawString(85,  ypos, buf);
 }
 
 // redraw entire display with the latest info
 static void showPowerInfo (const struct Payload* payload) {
   glcd.clear();
   glcd.setFont(font_clR6x6);
-  glcd.drawString_P(84,  0, PSTR("Solar W"));
-  glcd.drawString_P(84, 22, PSTR(" Home W"));
-  glcd.drawString_P(84, 44, PSTR("Cooking"));
+  glcd.drawString_P(85,  0, PSTR("Solar W"));
+  glcd.drawString_P(85, 22, PSTR(" Home W"));
+  glcd.drawString_P(85, 44, PSTR("Cooking"));
   glcd.setFont(font_8x13B);
   showPower(payload[0].tdiff, 52);  // cooking
   showPower(payload[1].tdiff, 8);   // solar
